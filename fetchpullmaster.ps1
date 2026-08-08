af3660cda819fc83d07c9bcf7bd9d5d01216e4dc
elquecosadeque <bleonp1997@gmail.com>
2026-08-07 23:17:55 -0500

    fetchpullmaster now supports main as fallback

--- fetchpullmaster.ps1
+++ fetchpullmaster.ps1
@@ -1,10 +1,15 @@
-git checkout master
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+$branch = 'master'
+git checkout $branch
+if ($LASTEXITCODE) {
+    $branch = 'main'
+    git checkout $branch
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
 
 git reset --hard HEAD
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
-git fetch origin master
+git fetch origin $branch
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
-git pull origin master
+git pull origin $branch
--- glist.ps1
+++ glist.ps1
@@ -1,7 +1,7 @@
 @(
     [pscustomobject]@{ Script = 'cdx.ps1';             Purpose = 'Move up one or more directories' }
     [pscustomobject]@{ Script = 'commit.ps1';          Purpose = 'Commit staged files using a message or the current branch name' }
-    [pscustomobject]@{ Script = 'fetchpullmaster.ps1'; Purpose = 'Reset and update the local master branch from origin' }
+    [pscustomobject]@{ Script = 'fetchpullmaster.ps1'; Purpose = 'Reset and update the local master or main branch from origin' }
     [pscustomobject]@{ Script = 'glist.ps1';           Purpose = 'List the scripts in this directory and their purpose' }
     [pscustomobject]@{ Script = 'gorepos.ps1';         Purpose = 'Change directory to the local repositories folder' }
     [pscustomobject]@{ Script = 'install.ps1';         Purpose = 'Add this directory to the user PATH' }
